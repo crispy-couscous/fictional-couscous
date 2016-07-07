781482162ccca93d139111f300ea7728a0a5d6fb
Niclas Willems <niclas.willems@gmail.com>
2016-07-07 16:45:50 -0400

    Added ajax call for foodPics import from Flickr.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import {flickrToken} from './credentials.js';
+var foodNode = $(".foodPics");
 
 var menu = $.ajax({
   url: 'https://json-data.herokuapp.com/restaurant/menu/1'
@@ -11,3 +13,27 @@ var news = $.ajax({
 }).then(function(response) {
   console.log(response);
 })
+
+
+function createFlickrTmpl(foodItem) {
+  var item = foodItem;
+  return `<img class="food-pic" src="https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg">`;
+};
+
+var foodPictures = $.ajax({
+  url: `https://api.flickr.com/services/rest/`,
+  data: {
+    method: "flickr.photos.search",
+    format: "json",
+    nojsoncallback: "1",
+    tags: "couscous",
+    per_page: "5",
+    api_key: flickrToken
+  }
+}).then(function(data){
+  console.log(data);
+  var foodItem = data.photos.photo.map(createFlickrTmpl);
+  console.log(foodItem);
+  console.log(foodNode);
+  foodNode.append(foodItem);
+});
