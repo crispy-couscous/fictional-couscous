d5ec85ce21044a71af26c59678142f34c8260359
Niclas Willems <niclas.willems@gmail.com>
2016-07-09 00:37:27 -0400

    Reservation functionality largely done. Additional styling completed.  Added margins to sides of container to play around with a cleaner appearance.  Easy to undo the CSS pending client review.

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,21 @@
 import $ from 'jquery';
-import {flickrToken} from './credentials.js';
-import {resData, confirmScreen, populateForm, blankForm} from './reservation.js';
+import {
+    flickrToken
+} from './credentials.js';
+import {
+    confirmScreen
+} from './reservation.js';
 
 var foodNode = $(".foodPics");
 
 function newsTmpl(data) {
-  var template = `
+    var template = `
       ${data.title}`
-  $('.newsContent').html(template)
+    $('.newsContent').html(template)
 };
 
-function appetizersTmpl (data) {
-  var template =`
+function appetizersTmpl(data) {
+    var template = `
   <div class="foodTitle">${data.item}<span class="foodPrice">${data.price}</span></div>
   <div class="foodBody"><div class="foodDescription">${data.description}</div>
   <div class="entreeInfo">
@@ -19,177 +23,163 @@ function appetizersTmpl (data) {
   <span class="entypo-flash"></span><span class="entypo-vimeo-circled"></span>
   </div></div></div>`
 
-  $('.appetizers').append(template)
+    $('.appetizers').append(template)
 };
 
-function entreesTmpl (data) {
-  var template =`
+function entreesTmpl(data) {
+    var template = `
   <div class="foodTitle">${data.item}<span class="foodPrice">${data.price}</span></div>
   <div class="foodBody"><div class="foodDescription">${data.description}</div>
   <div class="entreeInfo">
   <span class="entypo-alert"></span><span class="entypo-star"></span>
   <span class="entypo-flash"></span><span class="entypo-vimeo-circled"></span>
   </div></div></div>`
-  $('.entrees').append(template)
+    $('.entrees').append(template)
 };
 
-function sidesTmpl (data) {
-  var template =`
+function sidesTmpl(data) {
+    var template = `
   <div class="foodTitle">${data.item}<span class="foodPrice">${data.price}</span></div>
   <div class="foodBody"><div class="foodDescription">${data.description}</div>
   <div class="entreeInfo">
   <span class="entypo-alert"><span class="entypo-star"></span>
   <span class="entypo-flash"></span><span class="entypo-vimeo-circled"></span>
   </div></div></div>`
-  $('.sides').append(template)
+    $('.sides').append(template)
 };
 
 var menu = $.ajax({
-  url: 'https://json-data.herokuapp.com/restaurant/menu/1'
+    url: 'https://json-data.herokuapp.com/restaurant/menu/1'
 }).then(function(response) {
-  // console.log(response);
-  response.appetizers.forEach(appetizersTmpl)
-  response.entrees.forEach(entreesTmpl)
-  response.sides.forEach(sidesTmpl)
+    response.appetizers.forEach(appetizersTmpl)
+    response.entrees.forEach(entreesTmpl)
+    response.sides.forEach(sidesTmpl)
 })
 
 var news = $.ajax({
-  url: 'https://json-data.herokuapp.com/restaurant/news/1'
+    url: 'https://json-data.herokuapp.com/restaurant/news/1'
 }).then(function(response) {
-  var template = `
+    var template = `
         <div class="newsTitle">
           ${response.title} <span class="newsDate">${response.date_published}</span>
         </div>
         <div class="newsPost">
           <p>${response.post}</p>
         </div>`
-  $('.newsContent').append(template)
+    $('.newsContent').append(template)
 })
 
 $('#tabStory').on("click", function(event) {
-  event.preventDefault();
+    event.preventDefault();
 })
-// $('#tabMenu').on("click", function(event) {
-//   event.preventDefault();
-//   $('tabContent').html(menu)
-// })
-// $('#servation').on("click", function(event) {
-//   event.preventDefault();
-// })
 
 function createFlickrTmpl(foodItem) {
-  var item = foodItem;
-  return `<img class="food-pic" src="https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg">`;
+    var item = foodItem;
+    return `<img class="food-pic" src="https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg">`;
 };
 
 var foodPictures = $.ajax({
-  url: `https://api.flickr.com/services/rest/`,
-  data: {
-    method: "flickr.photos.search",
-    format: "json",
-    nojsoncallback: "1",
-    tags: "Mediterranean food",
-    per_page: "5",
-    api_key: flickrToken
-  }
-}).then(function(data){
-  var foodItem = data.photos.photo.map(createFlickrTmpl);
-  foodNode.append(foodItem);
+    url: `https://api.flickr.com/services/rest/`,
+    data: {
+        method: "flickr.photos.search",
+        format: "json",
+        nojsoncallback: "1",
+        tags: "Mediterranean food",
+        per_page: "5",
+        api_key: flickrToken
+    }
+}).then(function(data) {
+    var foodItem = data.photos.photo.map(createFlickrTmpl);
+    foodNode.append(foodItem);
 });
 
-function specialTmpl(item){
-  var special = item[0]
-  var template = `
+function specialTmpl(item) {
+    var special = item[0]
+    var template = `
   <div class="specialPic"><img src="http://cdn4.themediterraneandish.com/wp-content/uploads/2016/02/Spicy-Couscous-Recipe-with-Shrimp-and-Chorizo-10.jpg"></div>
   <div class="specialTitle">${special.item} <span class="specialPrice">${special.price}</span></div>
   <div class="specialDescription">${special.description}</div>
   `
-  $('.specials').append(template)
+    $('.specials').append(template)
 }
 
 function specialItem(menuId) {
-  console.log(menuId)
-  $.ajax({
-    url: 'https://json-data.herokuapp.com/restaurant/menu/1'
-  }).then(function(response) {
-    console.log(response.entrees)
-    var menuItem = response.entrees.filter(function(entree){
-      return entree.id === menuId;
+    $.ajax({
+        url: 'https://json-data.herokuapp.com/restaurant/menu/1'
+    }).then(function(response) {
+        var menuItem = response.entrees.filter(function(entree) {
+            return entree.id === menuId;
+        })
+        specialTmpl(menuItem)
     })
-    specialTmpl(menuItem)
-  })
 }
 
 $.ajax({
-  url: `https://json-data.herokuapp.com/restaurant/special/1`
+    url: `https://json-data.herokuapp.com/restaurant/special/1`
 }).then(function(response) {
-  specialItem(response.menu_item_id)
+    specialItem(response.menu_item_id)
 })
 
 // Tab listener below here
-var runCurrent = function (event) {
-  var tab = event.target.id;
+var runCurrent = function(event) {
+    var tab = event.target.id;
 
-  if (tab === "tabStory"){
-    $(".tabStoryBody").addClass( "current" );
-    $(".tabMenuBody").removeClass( "current" );
-    $(".tabReservationBody").removeClass( "current" );
+    if (tab === "tabStory") {
+        $(".tabStoryBody").addClass("current");
+        $(".tabMenuBody").removeClass("current");
+        $(".tabReservationBody").removeClass("current");
 
-    $(".tabStoryBody").removeClass( "notCurrent" );
-    $(".tabMenuBody").addClass( "notCurrent" );
-    $(".tabReservationBody").addClass( "notCurrent" );
+        $(".tabStoryBody").removeClass("notCurrent");
+        $(".tabMenuBody").addClass("notCurrent");
+        $(".tabReservationBody").addClass("notCurrent");
 
-    $(".tabStory").addClass( "currentTab" );
-    $(".tabMenu").removeClass( "currentTab" );
-    $(".tabReservation").removeClass( "currentTab" );
+        $(".tabStory").addClass("currentTab");
+        $(".tabMenu").removeClass("currentTab");
+        $(".tabReservation").removeClass("currentTab");
 
-    $(".tabStory").removeClass( "notCurrentTab" );
-    $(".tabMenu").addClass( "notCurrentTab" );
-    $(".tabReservation").addClass( "notCurrentTab" );
-  }
-  else if (tab === "tabMenu"){
-    $(".tabStoryBody").removeClass( "current" );
-    $(".tabMenuBody").addClass( "current" );
-    $(".tabReservationBody").removeClass( "current" );
+        $(".tabStory").removeClass("notCurrentTab");
+        $(".tabMenu").addClass("notCurrentTab");
+        $(".tabReservation").addClass("notCurrentTab");
+    } else if (tab === "tabMenu") {
+        $(".tabStoryBody").removeClass("current");
+        $(".tabMenuBody").addClass("current");
+        $(".tabReservationBody").removeClass("current");
 
-    $(".tabStoryBody").addClass( "notCurrent" );
-    $(".tabMenuBody").removeClass( "notCurrent" );
-    $(".tabReservationBody").addClass( "notCurrent" );
+        $(".tabStoryBody").addClass("notCurrent");
+        $(".tabMenuBody").removeClass("notCurrent");
+        $(".tabReservationBody").addClass("notCurrent");
 
-    $(".tabStory").removeClass( "currentTab" );
-    $(".tabMenu").addClass( "currentTab" );
-    $(".tabReservation").removeClass( "currentTab" );
+        $(".tabStory").removeClass("currentTab");
+        $(".tabMenu").addClass("currentTab");
+        $(".tabReservation").removeClass("currentTab");
 
-    $(".tabStory").addClass( "notCurrentTab" );
-    $(".tabMenu").removeClass( "notCurrentTab" );
-    $(".tabReservation").addClass( "notCurrentTab" );
-  }
-  else if (tab === "tabReservation"){
+        $(".tabStory").addClass("notCurrentTab");
+        $(".tabMenu").removeClass("notCurrentTab");
+        $(".tabReservation").addClass("notCurrentTab");
+    } else if (tab === "tabReservation") {
 
-    $(".tabStoryBody").removeClass( "current" );
-    $(".tabMenuBody").removeClass( "current" );
-    $(".tabReservationBody").addClass( "current" );
+        $(".tabStoryBody").removeClass("current");
+        $(".tabMenuBody").removeClass("current");
+        $(".tabReservationBody").addClass("current");
 
-    $(".tabStoryBody").addClass( "notCurrent" );
-    $(".tabMenuBody").addClass( "notCurrent" );
-    $(".tabReservationBody").removeClass( "notCurrent" );
+        $(".tabStoryBody").addClass("notCurrent");
+        $(".tabMenuBody").addClass("notCurrent");
+        $(".tabReservationBody").removeClass("notCurrent");
 
-    $(".tabStory").removeClass( "currentTab" );
-    $(".tabMenu").removeClass( "currentTab" );
-    $(".tabReservation").addClass( "currentTab" );
+        $(".tabStory").removeClass("currentTab");
+        $(".tabMenu").removeClass("currentTab");
+        $(".tabReservation").addClass("currentTab");
 
-    $(".tabStory").addClass( "notCurrentTab" );
-    $(".tabMenu").addClass( "notCurrentTab" );
-    $(".tabReservation").removeClass( "notCurrentTab" );
-
-    function populateForm(blankForm){
-      $(".resFormBody").append(blankForm);
+        $(".tabStory").addClass("notCurrentTab");
+        $(".tabMenu").addClass("notCurrentTab");
+        $(".tabReservation").removeClass("notCurrentTab");
     };
-  };
 };
+
+// Here be event listeners!
 
 $(".tabHeader").on('click', runCurrent);
 
 $('.formButton').on("click", function(event) {
-            confirmScreen(event)
-          });
+    confirmScreen(event)
+});
--- src/js/reservation.js
+++ src/js/reservation.js
@@ -1,38 +1,6 @@
 import $ from "jquery";
 
 var node = $(".resFormBody");
-var resData = "";
-var blankForm;
-
-function populateForm(blankForm) {
-  return `<div class="fullNameInfo">
-          <div class="formLabel"><label for="fullName">Full Name</label></div>
-          <div class="fullNameInput"><input type="text" id="fullName" name="user_name"></div>
-          </div>
-
-          <div class="numGuestsInfo">
-          <div class="formLabel"><label for="numGuests">Number of Guests</label></div>
-          <div class="guestInput"><input type="text" id="numGuests" name="num_Guests" /></div>
-          </div>
-
-          <div class="dateResInfo">
-          <div class="formLabel"><label for="dateRes">Date</label></div>
-          <div class="dateInput"><input type="date" value="" placeholder="" id="dateRes" name="date_res" /></div>
-          </div>
-
-          <div class="messageInfo">
-          <div class="formLabel"><label for="msg">Message</label></div>
-          <div class="messageInput"><textarea id="msg" name="user_message"></textarea></div>
-          </div>
-
-          <div class="seatPrefInfo">
-          <div class="formLabel"><label for="seatPref">Seating Preference</label></div>
-          <div class="seatInput"><select id="seatPref" name="seat_pref" />
-            <option value = "indoor">Indoor</option>
-            <option value = "outdoor">Outdoor</option>
-          </select></div>
-          </div>`
-}
 
 function confirmationMsg(){
   return`<span class="Thanks"><h3>Thank you for your reservation!
@@ -46,4 +14,4 @@ function confirmScreen(data) {
     return
 };
 
-export {resData, confirmScreen, populateForm, blankForm};
+export {confirmScreen};
